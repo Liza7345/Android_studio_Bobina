cfff93589ea8aa673c8db05c1e203bd19d46e493
BobinaLiza <bobinaliza14@gmail.com>
2026-01-31 10:53:13 +0300

    Version_2

--- app/src/main/java/com/example/first_app_Bobina/MainActivity.kt
+++ app/src/main/java/com/example/first_app_Bobina/MainActivity.kt
@@ -1,37 +1,33 @@
 package com.example.first_app_Bobina
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.first_app_Bobina.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var diceResult: TextView
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        diceResult = findViewById(R.id.diceResult)
-        // тип Button
-        val rollButton: Button = findViewById(R.id.btnStart)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
-    }
 
-    private fun rollDice() {
-        val diceValue = Random.nextInt(1, 7)
-        diceResult.text = diceValue.toString()
+        binding.btnStart.setOnClickListener {
+            val diceValue = Random.nextInt(1, 7)
+            binding.diceResult.text = diceValue.toString()
+        }
     }
 }
